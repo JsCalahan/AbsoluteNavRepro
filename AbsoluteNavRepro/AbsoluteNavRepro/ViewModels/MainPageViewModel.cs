1575eec17c80eeca9ef8a1749301b9f7df54be7b
Jeremy Calahan <jcalahan@peerlessproducts.com>
2020-01-22 15:54:26 -0600

    Add project files.

---
+++ AbsoluteNavRepro/AbsoluteNavRepro/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsoluteNavRepro.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+
+        public DelegateCommand AbsoluteNavigateCommand { get; set; }
+        public DelegateCommand NavigateCommand { get; set; }
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+
+
+            NavigateCommand = new DelegateCommand(async () =>
+            {
+                await Task.Delay(2000);
+                await NavigationService.NavigateAsync("MainPage");
+            });
+
+            AbsoluteNavigateCommand = new DelegateCommand(async () =>
+            {
+                await Task.Delay(2000);
+                await NavigationService.NavigateAsync("/MainPage");
+            });
+        }
+    }
+}
